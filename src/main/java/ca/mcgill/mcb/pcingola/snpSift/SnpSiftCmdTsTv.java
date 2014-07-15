b51e128a08cd5b5b6ccab57b39f29ac6d1669a2a
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-07-15 13:54:06 -0400

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpSift/SnpSiftCmdTsTv.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpSift/SnpSiftCmdTsTv.java
@@ -1,7 +1,6 @@
 package ca.mcgill.mcb.pcingola.snpSift;
 
 import ca.mcgill.mcb.pcingola.fileIterator.VcfFileIterator;
-import ca.mcgill.mcb.pcingola.interval.Genome;
 import ca.mcgill.mcb.pcingola.stats.TsTvStats;
 import ca.mcgill.mcb.pcingola.util.Timer;
 import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
@@ -16,7 +15,6 @@ public class SnpSiftCmdTsTv extends SnpSift {
 	public static final int SHOW_EVERY = 1000;
 	public static final int SHOW_EVERY_NL = 100 * SHOW_EVERY;
 
-	Genome genome;
 	TsTvStats tsTvStats;
 	String vcfFileName;
 
@@ -40,26 +38,9 @@ public class SnpSiftCmdTsTv extends SnpSift {
 	 */
 	@Override
 	public void parse(String[] args) {
-		if (args.length != 2) usage(null);
+		if (args.length < 1) usage(null);
 		int argc = 0;
-
-		// Homozygous, heterozygous or 'any' SNPs?
-		Boolean homozygous = null;
-		String homStr = args[argc++].toUpperCase();
-		// You can write 'ho' or 'homozygous'
-		if (homStr.startsWith("HO")) homozygous = true;
-		else if (homStr.startsWith("HE")) homozygous = false;
-		else if (homStr.startsWith("AN")) homozygous = null;
-		else usage("Expecting 'hom', 'het' or 'any', but got '" + args[3] + "'");
-
-		// Create stats object
-		tsTvStats = new TsTvStats(homozygous);
-
-		// Create genome
-		genome = new Genome("genome");
-
-		// VCF file
-		vcfFileName = args[argc++];
+		vcfFileName = args[argc++]; // VCF file
 	}
 
 	/**
@@ -69,7 +50,9 @@ public class SnpSiftCmdTsTv extends SnpSift {
 	public void run() {
 		Timer.showStdErr("Analysing '" + vcfFileName + "'");
 
-		VcfFileIterator vcfFile = new VcfFileIterator(vcfFileName, genome);
+		tsTvStats = new TsTvStats(); // Create stats object
+
+		VcfFileIterator vcfFile = new VcfFileIterator(vcfFileName);
 		vcfFile.setCreateChromos(true); // Create chromosomes when needed
 		vcfFile.setDebug(debug);
 
@@ -103,7 +86,7 @@ public class SnpSiftCmdTsTv extends SnpSift {
 	@Override
 	public void usage(String errMsg) {
 		if (errMsg != null) System.err.println("Error: " + errMsg);
-		System.err.println("Usage: java -jar " + SnpSift.class.getSimpleName() + "" + ".jar tstv <homozygous> file1.vcf\n\t<homozygous> : Use only 'hom', 'het', 'any' genotype fields (i.e. samples). \nWARNING: Only SNPs are used for Ts/Tv calculations.");
+		System.err.println("Usage: java -jar " + SnpSift.class.getSimpleName() + "" + ".jar tstv file1.vcf\nWARNING: Only SNPs are used for Ts/Tv calculations.");
 		System.exit(1);
 	}
 }
