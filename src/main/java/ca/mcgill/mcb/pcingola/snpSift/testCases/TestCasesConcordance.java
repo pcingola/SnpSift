9c7ff31a472b483d209a52f09c1b3adadbcfcaec
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-07-23 17:27:09 -0400

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesConcordance.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesConcordance.java
@@ -29,6 +29,22 @@ public class TestCasesConcordance extends TestCase {
 		return ssconc;
 	}
 
+	/**
+	 * Check that a all values match
+	 */
+	void checkConcordance(String refVcfFile, String vcfFile, CountByType count) {
+		SnpSiftCmdConcordance ssconc = checkConcordance(refVcfFile, vcfFile);
+		CountByType concordance = ssconc.getConcordance();
+
+		for (String key : count.keysSorted()) {
+			System.out.println("Checking\t'" + key + "' = " + count.get(key));
+			Assert.assertEquals(count.get(key), concordance.get(key));
+		}
+	}
+
+	/**
+	 * Check that a single value matches
+	 */
 	void checkConcordance(String refVcfFile, String vcfFile, String key, int value) {
 		SnpSiftCmdConcordance ssconc = checkConcordance(refVcfFile, vcfFile);
 		CountByType concordance = ssconc.getConcordance();
@@ -37,12 +53,41 @@ public class TestCasesConcordance extends TestCase {
 		Assert.assertEquals(value, concordance.get(key));
 	}
 
-	public void test_01() {
-		checkConcordance("test/concordance_ref_01.vcf", "test/concordance_test_01.vcf", "ALT_2/ALT_2", 1);
-	}
+	//	public void test_01() {
+	//		checkConcordance("test/concordance_ref_01.vcf", "test/concordance_test_01.vcf", "ALT_2/ALT_2", 1);
+	//	}
+	//
+	//	public void test_02() {
+	//		checkConcordance("test/concordance_ref_02.vcf", "test/concordance_test_02.vcf", "ALT_2/ALT_2", 2);
+	//	}
 
-	public void test_02() {
-		checkConcordance("test/concordance_ref_02.vcf", "test/concordance_test_02.vcf", "ALT_2/ALT_2", 2);
+	//	public void test_03() {
+	//		CountByType count = new CountByType();
+	//		count.inc("ALT_2/ALT_2", 1);
+	//		count.inc("ALT_2/REF", 1);
+	//		count.inc("ALT_2/MISSING_ENTRY_concordance_test_03", 1);
+	//
+	//		checkConcordance("test/concordance_ref_03.vcf", "test/concordance_test_03.vcf", count);
+	//	}
+
+	//	public void test_04() {
+	//		CountByType count = new CountByType();
+	//		count.inc("ALT_2/ALT_2", 1);
+	//		count.inc("ALT_2/REF", 1);
+	//		count.inc("ALT_2/MISSING_ENTRY_concordance_test_04", 1);
+	//		count.inc("ALT_2/MISSING_GT_concordance_test_04", 1);
+	//
+	//		checkConcordance("test/concordance_ref_04.vcf", "test/concordance_test_04.vcf", count);
+	//	}
+
+	public void test_05() {
+		CountByType count = new CountByType();
+		count.inc("ALT_2/ALT_2", 1);
+		count.inc("ALT_2/REF", 1);
+		count.inc("ALT_2/MISSING_ENTRY_concordance_test_05", 1);
+		count.inc("ALT_2/MISSING_GT_concordance_test_05", 1);
+
+		checkConcordance("test/concordance_ref_05.vcf", "test/concordance_test_05.vcf", count);
 	}
 
 }
