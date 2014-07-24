8b7ac0bb17f21ca0d29e902178bbc7756a249918
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-07-23 20:25:03 -0400

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesZzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesZzz.java
@@ -1,13 +1,12 @@
 package ca.mcgill.mcb.pcingola.snpSift.testCases;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
-import ca.mcgill.mcb.pcingola.snpSift.SnpSiftCmdFilter;
-import ca.mcgill.mcb.pcingola.util.Gpr;
+import ca.mcgill.mcb.pcingola.snpSift.SnpSiftCmdAnnotate;
 import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
-import ca.mcgill.mcb.pcingola.vcf.VcfGenotype;
 
 /**
  * Try test cases in this class before adding them to long test cases
@@ -16,34 +15,85 @@ import ca.mcgill.mcb.pcingola.vcf.VcfGenotype;
  */
 public class TestCasesZzz extends TestCase {
 
-	public static boolean debug = true;
+	public static boolean debug = false;
 	public static boolean verbose = true || debug;
 
 	protected String[] defaultExtraArgs = null;
 
 	/**
-	 * Filter by PL genottype tag
+	 * Annotate
 	 */
-	public void test_14() {
-		// Filter data
-		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
-		String expression = "( GEN[0].PL[1] > 10 ) ";
-		List<VcfEntry> list = snpsiftFilter.filter("test/test01.vcf", expression, true);
+	public List<VcfEntry> annotate(String dbFileName, String fileName, String[] extraArgs) {
+		System.out.println("Annotate: " + dbFileName + "\t" + fileName);
 
-		// Check that it satisfies the condition
-		System.out.println("Expression: '" + expression + "'");
-		Assert.assertNotNull(list);
-		Assert.assertTrue(list.size() > 0);
-		for (VcfEntry vcfEntry : list) {
-			if (verbose) System.out.println("\t" + vcfEntry);
+		// Create command line
+		String args[] = argsList(dbFileName, fileName, extraArgs);
 
-			VcfGenotype vcfGenotype = vcfEntry.getVcfGenotype(0);
-			String pl = vcfGenotype.get("PL");
-			String plSub = pl.split(",")[1];
-			int plSubInt = Gpr.parseIntSafe(plSub);
+		// Iterate over VCF entries
+		SnpSiftCmdAnnotate snpSiftAnnotate = new SnpSiftCmdAnnotate(args);
+		snpSiftAnnotate.setDebug(debug);
+		snpSiftAnnotate.setVerbose(verbose);
+		List<VcfEntry> results = snpSiftAnnotate.run(true);
 
-			Assert.assertTrue(plSubInt > 10);
+		// Check
+		Assert.assertTrue(results != null);
+		Assert.assertTrue(results.size() > 0);
+		return results;
+	}
+
+	public void annotateTest(String dbFileName, String fileName) {
+		annotateTest(dbFileName, fileName, null);
+	}
+
+	/**
+	 * Annotate a file and check that the new annotation matches the expected one
+	 */
+	public void annotateTest(String dbFileName, String fileName, String[] extraArgs) {
+		List<VcfEntry> results = annotate(dbFileName, fileName, extraArgs);
+
+		// Check each entry
+		for (VcfEntry vcf : results) {
+			// We expect the same annotation twice
+			String idstr = vcf.getId();
+
+			// Get expected IDs
+			String expectedIds = vcf.getInfo("EXP_IDS");
+			if (expectedIds != null) {
+				expectedIds = expectedIds.replace('|', ';');
+				if (expectedIds.equals(".")) expectedIds = "";
+
+				// Compare
+				Assert.assertEquals(expectedIds, idstr);
+			} else fail("EXP_IDS (expected ids) INFO field missing in " + fileName + ", entry:\n" + vcf);
 		}
+	}
+
+	protected String[] argsList(String dbFileName, String fileName, String[] extraArgs) {
+		ArrayList<String> argsList = new ArrayList<String>();
+
+		if (defaultExtraArgs != null) {
+			for (String arg : defaultExtraArgs)
+				argsList.add(arg);
+		}
+
+		if (extraArgs != null) {
+			for (String arg : extraArgs)
+				argsList.add(arg);
+		}
+
+		argsList.add(dbFileName);
+		argsList.add(fileName);
+		return argsList.toArray(new String[0]);
+	}
+
+	/**
+	 * Chromosomes in VCF file are called 'chr22' instead of '22'.
+	 * This should work OK as well.
+	 */
+	public void test_05() {
+		String dbFileName = "./test/db_test_chr22.vcf";
+		String fileName = "./test/test_chr22.vcf";
+		annotateTest(dbFileName, fileName);
 	}
 
 }
