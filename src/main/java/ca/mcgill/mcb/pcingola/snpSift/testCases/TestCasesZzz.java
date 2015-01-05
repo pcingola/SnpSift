213923ab6bf528d01c3afc78b9efd15cdb5bb510
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-01-05 11:30:27 -0500

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesZzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesZzz.java
@@ -8,7 +8,6 @@ import org.junit.Assert;
 
 import ca.mcgill.mcb.pcingola.snpSift.SnpSiftCmdFilter;
 import ca.mcgill.mcb.pcingola.util.Gpr;
-import ca.mcgill.mcb.pcingola.vcf.VcfEffect;
 import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
 
 /**
@@ -31,7 +30,7 @@ public class TestCasesZzz extends TestCase {
 
 		// Filter data
 		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
-		String expression = "EFF[0].EFFECT = 'synonymous_variant'";
+		String expression = "ANN[0].EFFECT = 'synonymous_variant'";
 		List<VcfEntry> list = snpsiftFilter.filter("test/test03.ann.vcf", expression, true);
 
 		// Check that it satisfies the condition
@@ -49,161 +48,161 @@ public class TestCasesZzz extends TestCase {
 		Assert.assertEquals(3, count); // Check that total number of lines is OK
 	}
 
-	/**
-	 * Filter by ANN[*].EFFECT (any effect)
-	 */
-	public void test_27_ann() {
-		Gpr.debug("Test");
-
-		// Filter data
-		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
-		String expression = "EFF[*].EFFECT = 'synonymous_variant'";
-		List<VcfEntry> list = snpsiftFilter.filter("test/test03.ann.vcf", expression, true);
-
-		// Check that it satisfies the condition
-		if (verbose) System.out.println("Expression: '" + expression + "'");
-		int count = 0;
-		for (VcfEntry vcfEntry : list) {
-			if (verbose) System.out.println("\t" + vcfEntry);
-
-			String effStr = vcfEntry.getInfo("ANN");
-			boolean any = false;
-			for (String eff : effStr.split(",")) {
-				String e = eff.split("\\|")[1];
-				if (e.equals("synonymous_variant")) {
-					count++;
-					any = true;
-				}
-			}
-
-			Assert.assertEquals(true, any); // Check that all lines match the expression
-		}
-
-		Assert.assertEquals(3, count); // Check that total number of lines is OK
-	}
-
-	/**
-	 * Filter by ANN[*].CODING
-	 */
-	public void test_32_ann() {
-		Gpr.debug("Test");
-
-		// Filter data
-		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
-		String expression = "EFF[*].BIOTYPE = 'protein_coding'";
-		List<VcfEntry> list = snpsiftFilter.filter("test/test03.ann.vcf", expression, true);
-
-		// Check that it satisfies the condition
-		if (verbose) System.out.println("Expression: '" + expression + "'");
-		int count = 0;
-		for (VcfEntry vcfEntry : list) {
-			if (verbose) System.out.println("\t" + vcfEntry);
-
-			boolean any = false;
-			String effStr = vcfEntry.getInfo("ANN");
-			for (String eff : effStr.split(",")) {
-				String e = eff.split("\\|", -1)[7];
-				if (e.equals("protein_coding")) {
-					count++;
-					any = true;
-				}
-			}
-			Assert.assertEquals(true, any); // Check that all lines match the expression
-		}
-
-		Assert.assertEquals(113, count); // Check that total number of lines is OK
-	}
-
-	/**
-	 * Filter by ANN[*].CODING
-	 */
-	public void test_33_ann() {
-		Gpr.debug("Test");
-
-		// Filter data
-		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
-		String expression = "EFF[*].BIOTYPE = 'lincRNA'";
-		List<VcfEntry> list = snpsiftFilter.filter("test/test03.ann.vcf", expression, true);
-
-		// Check that it satisfies the condition
-		if (verbose) System.out.println("Expression: '" + expression + "'");
-		int count = 0;
-		for (VcfEntry vcfEntry : list) {
-			if (verbose) System.out.println("\t" + vcfEntry);
-
-			boolean any = false;
-			String effStr = vcfEntry.getInfo("ANN");
-			for (String eff : effStr.split(",")) {
-				String e = eff.split("\\|", -1)[7];
-				any |= (e.equals("lincRNA"));
-			}
-
-			if (any) count++;
-			Assert.assertEquals(true, any); // Check that all lines match the expression
-		}
-
-		Assert.assertEquals(727, count); // Check that total number of lines is OK
-	}
-
-	/**
-	 * Filter by ANN[ALL].EFFECT
-	 */
-	public void test_34_ann() {
-		Gpr.debug("Test");
-
-		// Filter data
-		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
-		String expression = "(EFF[ALL].EFFECT = 'downstream_gene_variant')";
-		List<VcfEntry> list = snpsiftFilter.filter("test/downstream.ann.vcf", expression, true);
-
-		// Check that it satisfies the condition
-		if (verbose) System.out.println("Expression: '" + expression + "'");
-		int count = 0;
-		for (VcfEntry vcfEntry : list) {
-			if (verbose) System.out.println("\t" + vcfEntry);
-			boolean all = true;
-			String effStr = vcfEntry.getInfo("ANN");
-			for (String eff : effStr.split(",")) {
-				String e = eff.split("\\|")[1];
-				all &= e.equals("downstream_gene_variant");
-			}
-
-			if (!all) Gpr.debug("Error: " + effStr);
-			if (all) count++;
-			Assert.assertEquals(true, all); // Check that all lines match the expression
-		}
-
-		Assert.assertEquals(2, count); // Check that total number of lines is OK
-	}
-
-	/**
-	 * Filter by EFF[*].GENE
-	 */
-	public void test_35_ann() {
-		Gpr.debug("Test");
-
-		// Filter data
-		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
-		String expression = "EFF[*].GENE = 'BICD1'";
-		List<VcfEntry> list = snpsiftFilter.filter("test/test_gene.ann.vcf", expression, true);
-
-		// Check that it satisfies the condition
-		if (verbose) System.out.println("Expression: '" + expression + "'");
-		int count = 0;
-		for (VcfEntry vcfEntry : list) {
-			if (verbose) System.out.println("\t" + vcfEntry);
-
-			boolean any = false;
-			for (VcfEffect eff : vcfEntry.parseEffects(null)) {
-				Assert.assertEquals("BICD1", eff.getGeneName());
-				any = true;
-			}
-
-			if (any) count++;
-			Assert.assertEquals(true, any); // Check that all lines match the expression
-		}
-
-		Assert.assertEquals(1, count); // Check that total number of lines is OK
-	}
+	//	/**
+	//	 * Filter by ANN[*].EFFECT (any effect)
+	//	 */
+	//	public void test_27_ann() {
+	//		Gpr.debug("Test");
+	//
+	//		// Filter data
+	//		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
+	//		String expression = "EFF[*].EFFECT = 'synonymous_variant'";
+	//		List<VcfEntry> list = snpsiftFilter.filter("test/test03.ann.vcf", expression, true);
+	//
+	//		// Check that it satisfies the condition
+	//		if (verbose) System.out.println("Expression: '" + expression + "'");
+	//		int count = 0;
+	//		for (VcfEntry vcfEntry : list) {
+	//			if (verbose) System.out.println("\t" + vcfEntry);
+	//
+	//			String effStr = vcfEntry.getInfo("ANN");
+	//			boolean any = false;
+	//			for (String eff : effStr.split(",")) {
+	//				String e = eff.split("\\|")[1];
+	//				if (e.equals("synonymous_variant")) {
+	//					count++;
+	//					any = true;
+	//				}
+	//			}
+	//
+	//			Assert.assertEquals(true, any); // Check that all lines match the expression
+	//		}
+	//
+	//		Assert.assertEquals(3, count); // Check that total number of lines is OK
+	//	}
+	//
+	//	/**
+	//	 * Filter by ANN[*].CODING
+	//	 */
+	//	public void test_32_ann() {
+	//		Gpr.debug("Test");
+	//
+	//		// Filter data
+	//		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
+	//		String expression = "EFF[*].BIOTYPE = 'protein_coding'";
+	//		List<VcfEntry> list = snpsiftFilter.filter("test/test03.ann.vcf", expression, true);
+	//
+	//		// Check that it satisfies the condition
+	//		if (verbose) System.out.println("Expression: '" + expression + "'");
+	//		int count = 0;
+	//		for (VcfEntry vcfEntry : list) {
+	//			if (verbose) System.out.println("\t" + vcfEntry);
+	//
+	//			boolean any = false;
+	//			String effStr = vcfEntry.getInfo("ANN");
+	//			for (String eff : effStr.split(",")) {
+	//				String e = eff.split("\\|", -1)[7];
+	//				if (e.equals("protein_coding")) {
+	//					count++;
+	//					any = true;
+	//				}
+	//			}
+	//			Assert.assertEquals(true, any); // Check that all lines match the expression
+	//		}
+	//
+	//		Assert.assertEquals(113, count); // Check that total number of lines is OK
+	//	}
+	//
+	//	/**
+	//	 * Filter by ANN[*].CODING
+	//	 */
+	//	public void test_33_ann() {
+	//		Gpr.debug("Test");
+	//
+	//		// Filter data
+	//		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
+	//		String expression = "EFF[*].BIOTYPE = 'lincRNA'";
+	//		List<VcfEntry> list = snpsiftFilter.filter("test/test03.ann.vcf", expression, true);
+	//
+	//		// Check that it satisfies the condition
+	//		if (verbose) System.out.println("Expression: '" + expression + "'");
+	//		int count = 0;
+	//		for (VcfEntry vcfEntry : list) {
+	//			if (verbose) System.out.println("\t" + vcfEntry);
+	//
+	//			boolean any = false;
+	//			String effStr = vcfEntry.getInfo("ANN");
+	//			for (String eff : effStr.split(",")) {
+	//				String e = eff.split("\\|", -1)[7];
+	//				any |= (e.equals("lincRNA"));
+	//			}
+	//
+	//			if (any) count++;
+	//			Assert.assertEquals(true, any); // Check that all lines match the expression
+	//		}
+	//
+	//		Assert.assertEquals(727, count); // Check that total number of lines is OK
+	//	}
+	//
+	//	/**
+	//	 * Filter by ANN[ALL].EFFECT
+	//	 */
+	//	public void test_34_ann() {
+	//		Gpr.debug("Test");
+	//
+	//		// Filter data
+	//		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
+	//		String expression = "(EFF[ALL].EFFECT = 'downstream_gene_variant')";
+	//		List<VcfEntry> list = snpsiftFilter.filter("test/downstream.ann.vcf", expression, true);
+	//
+	//		// Check that it satisfies the condition
+	//		if (verbose) System.out.println("Expression: '" + expression + "'");
+	//		int count = 0;
+	//		for (VcfEntry vcfEntry : list) {
+	//			if (verbose) System.out.println("\t" + vcfEntry);
+	//			boolean all = true;
+	//			String effStr = vcfEntry.getInfo("ANN");
+	//			for (String eff : effStr.split(",")) {
+	//				String e = eff.split("\\|")[1];
+	//				all &= e.equals("downstream_gene_variant");
+	//			}
+	//
+	//			if (!all) Gpr.debug("Error: " + effStr);
+	//			if (all) count++;
+	//			Assert.assertEquals(true, all); // Check that all lines match the expression
+	//		}
+	//
+	//		Assert.assertEquals(2, count); // Check that total number of lines is OK
+	//	}
+	//
+	//	/**
+	//	 * Filter by EFF[*].GENE
+	//	 */
+	//	public void test_35_ann() {
+	//		Gpr.debug("Test");
+	//
+	//		// Filter data
+	//		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
+	//		String expression = "EFF[*].GENE = 'BICD1'";
+	//		List<VcfEntry> list = snpsiftFilter.filter("test/test_gene.ann.vcf", expression, true);
+	//
+	//		// Check that it satisfies the condition
+	//		if (verbose) System.out.println("Expression: '" + expression + "'");
+	//		int count = 0;
+	//		for (VcfEntry vcfEntry : list) {
+	//			if (verbose) System.out.println("\t" + vcfEntry);
+	//
+	//			boolean any = false;
+	//			for (VcfEffect eff : vcfEntry.parseEffects(null)) {
+	//				Assert.assertEquals("BICD1", eff.getGeneName());
+	//				any = true;
+	//			}
+	//
+	//			if (any) count++;
+	//			Assert.assertEquals(true, any); // Check that all lines match the expression
+	//		}
+	//
+	//		Assert.assertEquals(1, count); // Check that total number of lines is OK
+	//	}
 
 }
