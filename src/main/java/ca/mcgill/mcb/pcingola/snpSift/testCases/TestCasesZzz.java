7a1eea2b0a8fe39887a063928dd299985ba2fbe1
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-01-15 11:28:15 -0500

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesZzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesZzz.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
-import ca.mcgill.mcb.pcingola.snpSift.SnpSiftCmdExtractFields;
+import ca.mcgill.mcb.pcingola.snpSift.SnpSiftCmdFilter;
 import ca.mcgill.mcb.pcingola.util.Gpr;
+import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
 
 /**
  * Try test cases in this class before adding them to long test cases
@@ -17,48 +18,37 @@ public class TestCasesZzz extends TestCase {
 	public static boolean debug = false;
 	public static boolean verbose = false || debug;
 
-	protected String[] defaultExtraArgs = null;
-
 	/**
-	 * Extract fields and return the output lines
+	 * Filter by ANN[*].EFFECT (any effect)
 	 */
-	List<String> extract(String vcfFileName, String fieldExpression) {
-		String args[] = { vcfFileName, fieldExpression };
-		SnpSiftCmdExtractFields ssef = new SnpSiftCmdExtractFields(args);
+	public void test_27_ann() {
+		Gpr.debug("Test");
+		verbose = true;
 
-		List<String> linesList = ssef.run(true);
+		// Filter data
+		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
+		String expression = "ANN[*].EFFECT = 'missense_variant'";
+		List<VcfEntry> list = snpsiftFilter.filter("test/test03.ann.vcf", expression, true);
 
-		if (debug) {
-			for (String line : linesList)
-				Gpr.debug(line);
+		// Check that it satisfies the condition
+		if (verbose) System.out.println("Expression: '" + expression + "'");
+		int count = 0;
+		for (VcfEntry vcfEntry : list) {
+			if (verbose) System.out.println("\t" + vcfEntry);
+
+			String effStr = vcfEntry.getInfo("ANN");
+			boolean any = false;
+			for (String eff : effStr.split(",")) {
+				String e = eff.split("\\|")[1];
+				if (e.equals("missense_variant")) {
+					count++;
+					any = true;
+				}
+			}
+
+			Assert.assertEquals(true, any); // Check that all lines match the expression
 		}
 
-		return linesList;
+		Assert.assertEquals(6, count); // Check that total number of lines is OK
 	}
-
-	/**
-	 * Extract fields form a file and check that the line matches (only one line expected from the file)
-	 */
-	void extractAndCheck(String vcfFileName, String fieldExpression, String expected) {
-		List<String> linesList = extract(vcfFileName, fieldExpression);
-		if (linesList.size() != 1) throw new RuntimeException("Only one line expected");
-		Assert.assertEquals(expected, linesList.get(0));
-	}
-
-	/**
-	 * Extract fields using sample names
-	 */
-	public void test_31() {
-		Gpr.debug("Test");
-		extractAndCheck("test/extractFields_31.vcf", "EFF[*].AA", "c.*568C>A");
-	}
-
-	/**
-	 * Extract fields using sample names
-	 */
-	public void test_31_ann() {
-		Gpr.debug("Test");
-		extractAndCheck("test/extractFields_31.ann.vcf", "ANN[*].CODON", "c.*568C>A");
-	}
-
 }
