40e13db2f36d22963b3d371ccd8b88102ae8cb91
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-01-15 11:43:09 -0500

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesFilter.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesFilter.java
@@ -763,6 +763,40 @@ public class TestCasesFilter extends TestCase {
 	}
 
 	/**
+	 * Filter by ANN[*].EFFECT (any effect)
+	 */
+	public void test_27_ann2() {
+		Gpr.debug("Test");
+		verbose = true;
+
+		// Filter data
+		SnpSiftCmdFilter snpsiftFilter = new SnpSiftCmdFilter();
+		String expression = "ANN[*].EFFECT = 'missense_variant'";
+		List<VcfEntry> list = snpsiftFilter.filter("test/test03.ann.vcf", expression, true);
+
+		// Check that it satisfies the condition
+		if (verbose) System.out.println("Expression: '" + expression + "'");
+		int count = 0;
+		for (VcfEntry vcfEntry : list) {
+			if (verbose) System.out.println("\t" + vcfEntry);
+
+			String effStr = vcfEntry.getInfo("ANN");
+			boolean any = false;
+			for (String eff : effStr.split(",")) {
+				String e = eff.split("\\|")[1];
+				if (e.equals("missense_variant")) {
+					count++;
+					any = true;
+				}
+			}
+
+			Assert.assertEquals(true, any); // Check that all lines match the expression
+		}
+
+		Assert.assertEquals(6, count); // Check that total number of lines is OK
+	}
+
+	/**
 	 * Test countHom function
 	 */
 	public void test_28() {
