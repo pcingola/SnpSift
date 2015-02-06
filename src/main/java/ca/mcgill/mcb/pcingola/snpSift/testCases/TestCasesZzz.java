613603a1539794b3cccefcc03463e8ca155ab2cf
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-02-06 11:45:18 -0300

    Annotate: Adding test case for checking that '-name' command line options modifies the headers accordingly

--- src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesZzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesZzz.java
@@ -1,9 +1,13 @@
 package ca.mcgill.mcb.pcingola.snpSift.testCases;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.Assert;
 import junit.framework.TestCase;
-import ca.mcgill.mcb.pcingola.snpSift.SnpSiftCmdConcordance;
-import ca.mcgill.mcb.pcingola.stats.CountByType;
+import ca.mcgill.mcb.pcingola.snpSift.SnpSiftCmdAnnotate;
+import ca.mcgill.mcb.pcingola.util.Gpr;
+import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
 
 /**
  * Try test cases in this class before adding them to long test cases
@@ -15,46 +19,122 @@ public class TestCasesZzz extends TestCase {
 	public static boolean debug = false;
 	public static boolean verbose = false || debug;
 
-	SnpSiftCmdConcordance checkConcordance(String refVcfFile, String vcfFile) {
-		if (verbose) System.err.println("\n\nConcordance between: " + refVcfFile + "\t" + vcfFile);
-		String args[] = { refVcfFile, vcfFile };
+	protected String[] defaultExtraArgs = null;
 
-		SnpSiftCmdConcordance ssconc = new SnpSiftCmdConcordance(args);
-		ssconc.setVerbose(verbose);
-		ssconc.setSuppressOutput(!verbose);
-		ssconc.setDebug(debug);
-		ssconc.setWriteBySampleFile(false);
-		ssconc.setWriteSummaryFile(false);
-		ssconc.run();
+	/**
+	 * Annotate
+	 */
+	public List<VcfEntry> annotate(String dbFileName, String fileName, String[] extraArgs) {
+		if (verbose) System.out.println("Annotate: " + dbFileName + "\t" + fileName);
 
-		CountByType concordance = ssconc.getConcordance();
-		if (verbose) System.out.println("\n\nConcordance:\n" + concordance);
+		// Create command line
+		String args[] = argsList(dbFileName, fileName, extraArgs);
 
-		return ssconc;
+		// Iterate over VCF entries
+		SnpSiftCmdAnnotate snpSiftAnnotate = new SnpSiftCmdAnnotate(args);
+		snpSiftAnnotate.setDebug(debug);
+		snpSiftAnnotate.setVerbose(verbose);
+		snpSiftAnnotate.setSuppressOutput(!verbose);
+		List<VcfEntry> results = snpSiftAnnotate.run(true);
+
+		// Check
+		Assert.assertTrue(results != null);
+		Assert.assertTrue(results.size() > 0);
+		return results;
 	}
 
 	/**
-	 * Check that a all values match
+	 * Annotate and return STDOUT as a string
 	 */
-	void checkConcordance(String refVcfFile, String vcfFile, CountByType count) {
-		SnpSiftCmdConcordance ssconc = checkConcordance(refVcfFile, vcfFile);
-		CountByType concordance = ssconc.getConcordance();
+	public String annotateOut(String dbFileName, String fileName, String[] extraArgs) {
+		if (verbose) System.out.println("Annotate: " + dbFileName + "\t" + fileName);
 
-		for (String key : count.keysSorted()) {
-			if (verbose) System.out.println("Checking\t'" + key + "'\tExpected: " + count.get(key) + "\tActual: " + concordance.get(key));
-			Assert.assertEquals(count.get(key), concordance.get(key));
+		// Create command line
+		String args[] = argsList(dbFileName, fileName, extraArgs);
+
+		// Iterate over VCF entries
+		SnpSiftCmdAnnotate snpSift = new SnpSiftCmdAnnotate(args);
+		snpSift.setDebug(debug);
+		snpSift.setVerbose(verbose);
+		snpSift.setSaveOutput(true);
+		snpSift.run();
+
+		// Check
+		return snpSift.getOutput();
+	}
+
+	public void annotateTest(String dbFileName, String fileName) {
+		annotateTest(dbFileName, fileName, null);
+	}
+
+	/**
+	 * Annotate a file and check that the new annotation matches the expected one
+	 */
+	public void annotateTest(String dbFileName, String fileName, String[] extraArgs) {
+		List<VcfEntry> results = annotate(dbFileName, fileName, extraArgs);
+
+		// Check each entry
+		for (VcfEntry vcf : results) {
+			// We expect the same annotation twice
+			String idstr = vcf.getId();
+
+			// Get expected IDs
+			String expectedIds = vcf.getInfo("EXP_IDS");
+			if (expectedIds != null) {
+				expectedIds = expectedIds.replace('|', ';');
+				if (expectedIds.equals(".")) expectedIds = "";
+
+				// Compare
+				Assert.assertEquals(expectedIds, idstr);
+			} else fail("EXP_IDS (expected ids) INFO field missing in " + fileName + ", entry:\n" + vcf);
 		}
 	}
 
-	/**
-	 * Check that a single value matches
-	 */
-	void checkConcordance(String refVcfFile, String vcfFile, String key, int value) {
-		SnpSiftCmdConcordance ssconc = checkConcordance(refVcfFile, vcfFile);
-		CountByType concordance = ssconc.getConcordance();
+	protected String[] argsList(String dbFileName, String fileName, String[] extraArgs) {
+		ArrayList<String> argsList = new ArrayList<String>();
 
-		if (verbose) System.out.println("Checking\t'" + key + "'\tExpected: " + value + "\tActual: " + concordance.get(key));
-		Assert.assertEquals(value, concordance.get(key));
+		if (defaultExtraArgs != null) {
+			for (String arg : defaultExtraArgs)
+				argsList.add(arg);
+		}
+
+		if (extraArgs != null) {
+			for (String arg : extraArgs)
+				argsList.add(arg);
+		}
+
+		argsList.add(dbFileName);
+		argsList.add(fileName);
+		return argsList.toArray(new String[0]);
+	}
+
+	/**
+	 * Annotate using "reserved" VCF fields (e.g. "AA" )
+	 * Header should be added if it doesn't exits.
+	 */
+	public void test_24() {
+		Gpr.debug("Test");
+		verbose = true;
+
+		String dbFileName = "./test/db_test_24.vcf";
+		String fileName = "./test/annotate_24.vcf";
+		String infoName = "PREPEND_";
+		String extraArgs[] = { "-name", infoName };
+
+		// Annotate
+		String out = annotateOut(dbFileName, fileName, extraArgs);
+
+		// Make sure output header for "PREPEND_AA"  and "PREPEND_BB" are present ONLY ONCE.
+		if (verbose) System.out.println(out);
+		int hasAa = 0;
+		int hasBb = 0;
+		for (String line : out.split("\n")) {
+			if (line.startsWith("##INFO=<ID=PREPEND_AA")) hasAa++;
+			if (line.startsWith("##INFO=<ID=PREPEND_BB")) hasBb++;
+		}
+
+		Assert.assertEquals(1, hasAa);
+		Assert.assertEquals(1, hasBb);
 	}
 
 }
