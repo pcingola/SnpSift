8e93db29a3d4bd860b30c62f9f7f3f3e2e092a53
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-03-23 09:51:21 -0400

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpSift/SnpSift.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpSift/SnpSift.java
@@ -469,7 +469,7 @@ public class SnpSift {
 				+ "\n\tvarType       : Annotate variant type (SNP,MNP,INS,DEL or MIXED)." //
 				+ "\n\tvcfCheck      : Check that VCF file is well formed." //
 				+ "\n\tvcf2tped      : Convert VCF to TPED." //
-		);
+				);
 
 		usageGenericAndDb();
 
@@ -489,7 +489,7 @@ public class SnpSift {
 				+ "\t-noLog               : Do not report usage statistics to server.\n" //
 				+ "\t-h                   : Help.\n" //
 				+ "\t-v                   : Verbose.\n" //
-		);
+				);
 	}
 
 	/**
