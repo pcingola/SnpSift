e57db6e6a56a44a2a09b9361ca1e763e62dc01c0
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-06-09 15:49:09 -0400

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesZzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpSift/testCases/TestCasesZzz.java
@@ -108,12 +108,23 @@ public class TestCasesZzz extends TestCase {
 		return argsList.toArray(new String[0]);
 	}
 
+	//	/**
+	//	 * Issue when database has REF including 'N' bases
+	//	 * WARNING: FIXING THIS REQUIRES A MAJOR CHANGE IN TH WAY WE ANNOTATE 
+	//	 *          VARIANTS (WON'T FIX NOW, BUT NEEDS TO BE FIXED IN THE FUTURE)
+	//	 */
+	//	public void test_30_annotate_N_reference_in_db() {
+	//		Gpr.debug("Test");
+	//		String dbFileName = "./test/db_test_30.vcf";
+	//		String fileName = "./test/annotate_30.vcf";
+	//		annotateTest(dbFileName, fileName);
+	//	}
+
 	/**
-	 * Issue when database has REF including 'N' bases
-	 * WARNING: FIXING THIS REQUIRES A MAJOR CHANGE IN TH WAY WE ANNOTATE 
-	 *          VARIANTS (WON'T FIX NOW, BUT NEEDS TO BE FIXED IN THE FUTURE)
+	 * Issue when database has REF several variants which have to 
+	 * be converted into minimal representation
 	 */
-	public void test_30_annotate_N_reference_in_db() {
+	public void test_31_annotate_minimal_representation_db() {
 		Gpr.debug("Test");
 		String dbFileName = "./test/db_test_30.vcf";
 		String fileName = "./test/annotate_30.vcf";
